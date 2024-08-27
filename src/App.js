c66cc9d3826e76620c161e814644aa8e7998e713
grethaga <163857709+grethaga@users.noreply.github.com>
2024-08-27 13:41:51 +0200

    added search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <div className="container">
+        <h1>Hello</h1>
+        <main>
+          <Dictionary />
+        </main>
+        <footer>Coded by Gretha Gall</footer>
+      </div>
     </div>
   );
 }
